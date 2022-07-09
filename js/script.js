0ee8fd06a2a984ef341c0bc3484057093f133abb
Maxim <96632620+Hoshivsky1@users.noreply.github.com>
2022-07-09 16:16:15 +0300

    :D

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
     modal('[data-modal]', '.modal', modalTimerId);
-    timer('.timer', '2022-08-11');
+    timer('.timer', '2022-11-29');
     cards();
     calc();
     forms('form', modalTimerId);
